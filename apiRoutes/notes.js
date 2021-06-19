11ad239e38a6686af96e57a736ea1672aafa4d48
J2021T <80201567+J2021T@users.noreply.github.com>
2021-06-19 18:33:51 -0400

    set up router.post method

--- apiRoutes/notes.js
+++ apiRoutes/notes.js
@@ -1,13 +1,24 @@
 const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
-const { notes } = require('../data/notes');
+
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, notes));
+    res.sendFile(path.join(__dirname, '../data/notes.json'));
 });
 
 router.post('/notes', (req, res) => {
-    let saved = JSON.parse(fs.readFileSync(notes, 'utf-8'));
+    let saved = JSON.parse(fs.readFileSync('./data/notes.json', 'utf8'));
+    let newNote = req.body;
+    newNote.id = saved.length;
+    console.log(newNote);
+    saved.push(newNote);
+    
+    fs.writeFile('./data/notes.json', JSON.stringify(saved), (err) => {
+        if (err) throw err;
+        console.log('The file has been updated');
+        res.json(saved);
+    });
 });
 
 module.exports = router;
