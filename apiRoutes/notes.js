3c0f99ac7a269553bbf7497cfd8c41a509050878
J2021T <80201567+J2021T@users.noreply.github.com>
2021-06-19 19:58:52 -0400

    application ready to go

--- apiRoutes/notes.js
+++ apiRoutes/notes.js
@@ -2,18 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 
-// gets save notes
+// gets saved notes
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../data/notes.json'));
 });
-
 
 // sets up the route to add new notes to saved notes
 router.post('/notes', (req, res) => {
     let saved = JSON.parse(fs.readFileSync('./data/notes.json', 'utf8'));
     let newNote = req.body;
     newNote.id = saved.length;
-    console.log(newNote);
     saved.push(newNote);
     
     fs.writeFile('./data/notes.json', JSON.stringify(saved), (err) => {
@@ -27,9 +25,16 @@ router.post('/notes', (req, res) => {
 router.delete('/notes/:id', (req, res) => {
     let saved = JSON.parse(fs.readFileSync('./data/notes.json', 'utf8'));
     let noteID = req.params.id;
-    console.log(saved, noteID);
     saved.splice(noteID, 1);
-    console.log(saved)
+    saved.forEach((note, index) => {
+        note.id = index;
+    });
+
+    fs.writeFile('./data/notes.json', JSON.stringify(saved), (err) => {
+        if (err) throw err;
+        console.log('The note has been deleted!');
+        res.json(saved);
+    });
 });
 
 module.exports = router;
