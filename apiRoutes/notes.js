c2bc785ad1e9d4c748581f10e0e0a98166aeb58e
J2021T <80201567+J2021T@users.noreply.github.com>
2021-06-19 18:54:38 -0400

    working on delete functionality

--- apiRoutes/notes.js
+++ apiRoutes/notes.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 
-
+// gets save notes
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../data/notes.json'));
 });
 
+
+// sets up the route to add new notes to saved notes
 router.post('/notes', (req, res) => {
     let saved = JSON.parse(fs.readFileSync('./data/notes.json', 'utf8'));
     let newNote = req.body;
@@ -19,6 +21,15 @@ router.post('/notes', (req, res) => {
         console.log('The file has been updated');
         res.json(saved);
     });
+});
+
+// deletes saved notes
+router.delete('/notes/:id', (req, res) => {
+    let saved = JSON.parse(fs.readFileSync('./data/notes.json', 'utf8'));
+    let noteID = req.params.id;
+    console.log(saved, noteID);
+    saved.splice(noteID, 1);
+    console.log(saved)
 });
 
 module.exports = router;
